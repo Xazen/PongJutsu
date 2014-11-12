16fd060bdf9bc24833299c5a0c8a23cc017f04ea
Jannik Szwaczka <blodjer@live.de>
2014-11-12 17:09:20 +0100

    Change shooting mechanics

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -8,24 +8,26 @@ namespace PongJutsu
 
 		public int speed;
 		public int damage;
-		private Vector2 movement;
+		private Vector2 movement = new Vector2(0,0);
 
 		public Color color1;
 		public Color color2;
 
 		[HideInInspector] public GameObject owner;
-
-		[HideInInspector] public int direction;
 		
 		private bool bounceBack = false;
 
-
 		void Start()
 		{
+			owner.GetComponentInChildren<PlayerAttack>().shotCount++;
 			colorSetup();
+		}
 
+		public void setInitialMovement(int directionX, float movementY)
+		{
 			// Set initial movement
-			movement.x = speed * direction;
+			movement.x = speed * directionX;
+			movement.y = movementY;
 		}
 
 		void colorSetup()
