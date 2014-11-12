67344456cd686b45ae185b93c0e40a83bf89973b
Jannik Szwaczka <blodjer@live.de>
2014-11-12 09:57:18 +0100

    Implement item base class

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PongJutsu
+{
+	public class Item : MonoBehaviour 
+	{
+
+		public virtual void content(Collider2D col)
+		{
+			
+		}
+
+		void OnTriggerEnter2D(Collider2D col)
+		{
+			if (col.GetComponent<Shot>() != null)
+			{
+				content(col);
+			}
+
+			Destroy(this.gameObject);
+		}
+	}
+}
+
