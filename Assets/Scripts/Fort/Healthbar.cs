04dd23592995273d65a595a3b04642353e99573c
Blodjer <blodjer@live.de>
2014-11-16 19:13:44 +0100

    Add changeable healthbar colors

--- Assets/Scripts/Fort/Healthbar.cs
+++ Assets/Scripts/Fort/Healthbar.cs
@@ -5,10 +5,13 @@ namespace PongJutsu
 {
 	public class Healthbar : MonoBehaviour
 	{
+		public Color colorFullHealth = Color.green;
+		public Color colorLowHealth = Color.red;
+
 		public void updateHealthbar(int health)
 		{
 			this.transform.localScale = new Vector2(1, health / 100f);
-			this.GetComponent<SpriteRenderer>().color = Color.Lerp(Color.green, Color.red, 1 - health / 100f);
+			this.GetComponent<SpriteRenderer>().color = Color.Lerp(colorFullHealth, colorLowHealth, 1 - health / 100f);
 		}
 	}
 }
