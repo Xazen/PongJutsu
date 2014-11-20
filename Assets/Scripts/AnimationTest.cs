fdf8db44b7ca4477b85fba73d8194918573ce32c
Jannik Szwaczka <blodjer@live.de>
2014-11-20 15:18:37 +0100

    Add animations test
    test destroyable forts animation
    test player animations

---
+++ Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PongJutsu
+{
+	public class AnimationTest : MonoBehaviour
+	{
+
+		public GameObject shotObject;
+		Animator animator;
+
+		void Start()
+		{
+			animator = this.GetComponent<Animator>();
+		}
+
+		void Update() 
+		{
+			if (Input.GetButtonDown("PlayerLeft shoot"))
+			{
+				animator.SetTrigger("Shoot");
+			}
+		}
+
+		public void Shoot()
+		{
+			GameObject shotInstance = (GameObject)Instantiate(shotObject, this.transform.position, new Quaternion());
+			shotInstance.GetComponent<Shuriken>().setInitialMovement(1, 0);
+		}
+	}
+
+}
