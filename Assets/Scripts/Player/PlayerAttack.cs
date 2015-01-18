ad3da3afbad32debfd6295a37ecfc71ee3091d8a
Blodjer <blodjer@live.de>
2015-01-18 17:27:44 +0100

    Add dynamic glow animation

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -24,9 +24,23 @@ namespace PongJutsu
 
 		void Update()
 		{
+			setGlow();
+
 			if (GameManager.allowInput)
-			{
 				Shooting();
+		}
+
+		void setGlow()
+		{
+			if (shotCount < maxActiveShots)
+			{
+				if (this.transform.parent.FindChild("Ninja").FindChild("Glow").gameObject.active == false)
+					this.transform.parent.FindChild("Ninja").FindChild("Glow").gameObject.SetActive(true);
+			}
+			else
+			{
+				if (this.transform.parent.FindChild("Ninja").FindChild("Glow").gameObject.active == true)
+					this.transform.parent.FindChild("Ninja").FindChild("Glow").gameObject.SetActive(false);
 			}
 		}
 
