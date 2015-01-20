b6732a4a5f3ef8e870459b284096be5c3068121f
Blodjer <blodjer@live.de>
2015-01-20 18:25:49 +0100

    Created sound manager class

---
+++ Assets/Scripts/Game/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PongJutsu
+{
+	public class SoundManager : MonoBehaviour
+	{
+
+		public Clip[] clips;
+
+		[System.Serializable]
+		public class Clip
+		{
+			public AudioClip clip;
+			public SoundState state;
+		}
+
+		public static void changeState(SoundState state)
+		{
+			Debug.Log("play state: " + state);
+		}
+	}
+}
+
+public enum SoundState
+{
+	MainMenu,
+	InGame	
+}
