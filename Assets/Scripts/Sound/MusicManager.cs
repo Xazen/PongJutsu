7afd0b93a7bd7f898a1702820f73dabb944721ff
Blodjer <blodjer@live.de>
2015-01-27 10:51:34 +0100

    Prepare musicmanager script

--- Assets/Scripts/Sound/MusicManager.cs
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -6,23 +6,21 @@ namespace PongJutsu
 {
 	public class MusicManager : MonoBehaviour
 	{
-		[SerializeField] private SoundState defaultState = SoundState.MainMenu;
-		[SerializeField] private StateClip[] clips;
+		[SerializeField] private Layer[] layers;
 
 		public static MusicManager current;
 
 		[System.Serializable]
-		private class StateClip
+		private class Layer
 		{
-			public AudioClip clip = null;
-			public SoundState state = 0;
+			public AudioSource source;
+			public AudioClip[] clips;
 		}
 
 
 		void Awake()
 		{
 			current = this;
-			PlayState(defaultState);
 		}
 
 		void Update()
@@ -49,27 +47,9 @@ namespace PongJutsu
 			}
 		}
 
-		public void PlayState(SoundState state)
-		{
-			PlayClip(getRandomClip(state));
-		}
-
 		public void FadeOut(float duration)
 		{
 			StartCoroutine(IFadeout(duration));
-		}
-
-		public void FadeToState(float duration, SoundState state)
-		{
-			StartCoroutine(IFadeToState(duration, state));
-		}
-
-		AudioClip getRandomClip(SoundState state)
-		{
-			List<StateClip> clipsList = new List<StateClip>(clips);
-			StateClip[] stateClips = clipsList.FindAll(x => x.state == state).ToArray();
-
-			return stateClips[Random.Range(0, stateClips.Length - 1)].clip;
 		}
 
 		void PlayClip(AudioClip clip)
@@ -93,24 +73,5 @@ namespace PongJutsu
 				yield return new WaitForEndOfFrame();
 			}
 		}
-
-		IEnumerator IFadeToState(float duration, SoundState state)
-		{
-			AudioSource audio = this.GetComponent<AudioSource>();
-
-			while (audio.volume > 0f)
-			{
-				float volume = audio.volume - (1f / duration) * Time.deltaTime;
-				audio.volume = Mathf.Clamp(volume, 0f, 1f);
-				yield return new WaitForEndOfFrame();
-			}
-
-			PlayState(state);
-		}
 	}
-}
-public enum SoundState
-{
-	MainMenu,
-	InGame	
 }
