eb38582fb72a8d8a94b2de77d5703bff0c82905e
Jannik Szwaczka <blodjer@live.de>
2015-01-29 09:44:34 +0100

    Develop help screen

--- Assets/Scripts/UI/HelpScreen.cs
+++ Assets/Scripts/UI/HelpScreen.cs
@@ -6,12 +6,38 @@ namespace PongJutsu
 {
 	public class HelpScreen : UIScript
 	{
+		[SerializeField] private Sprite[] helpImage;
+
+		private int currentIndex = 0;
+
+		private Image content;
+
+		public override void uiEnable()
+		{
+			base.uiEnable();
+
+			if (content == null)
+				content = this.transform.FindChild("Content").GetComponent<Image>();
+
+			content.sprite = helpImage[currentIndex];
+		}
+
 		public override void uiUpdate()
 		{
 			base.uiUpdate();
 
 			if (Input.anyKeyDown)
 				ui.SetTrigger("Back");
+		}
+
+		public void back()
+		{
+
+		}
+
+		public void next()
+		{
+
 		}
 	}
 }
