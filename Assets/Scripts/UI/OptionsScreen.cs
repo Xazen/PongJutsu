22a052abf1410d6a6239a191dc4322900d096f1f
Blodjer <blodjer@live.de>
2015-02-01 13:04:04 +0100

    Prepare OptionsScreen

---
+++ Assets/Scripts/UI/OptionsScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace PongJutsu
+{
+	public class OptionsScreen : UIScript 
+	{
+		public override void uiEnable()
+		{
+			base.uiEnable();
+
+			this.transform.FindChild("sl_MasterVolume").GetComponent<Slider>().value = GameOptions.masterVolume;
+			this.transform.FindChild("sl_MusicVolume").GetComponent<Slider>().value = GameOptions.musicVolume;
+		}
+
+		public void slide_MasterVolume(float value)
+		{
+			GameOptions.masterVolume = value;
+		}
+
+		public void slide_MusicVolume(float value)
+		{
+			GameOptions.musicVolume = value;
+		}
+	}
+}
