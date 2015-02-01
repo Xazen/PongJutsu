5835222d5f8c744143154d6de9bf7dd19249300a
Blodjer <blodjer@live.de>
2015-02-01 22:35:23 +0100

    Add help page indicator

--- Assets/Scripts/UI/HelpScreen.cs
+++ Assets/Scripts/UI/HelpScreen.cs
@@ -7,10 +7,27 @@ namespace PongJutsu
 	public class HelpScreen : UIScript
 	{
 		[SerializeField] private Sprite[] helpImage;
+		[SerializeField] private GameObject pageIndicator;
 
 		private int currentIndex = 0;
 
+		private bool blockScrolling;
+
 		private Image content;
+
+		void Start()
+		{
+			for (int i = 0; i < helpImage.Length; i++)
+			{
+				GameObject indicator = (GameObject)Instantiate(pageIndicator);
+				indicator.transform.SetParent(this.transform.FindChild("Indicators"));
+
+				float width = indicator.GetComponent<RectTransform>().rect.width;
+				indicator.GetComponent<RectTransform>().anchoredPosition = new Vector2(i * width - (width * helpImage.Length) / 2f, 0f);
+			}
+
+			setPageIndex(currentIndex);
+		}
 
 		public override void uiEnable()
 		{
@@ -19,27 +36,49 @@ namespace PongJutsu
 			if (content == null)
 				content = this.transform.FindChild("Content").GetComponent<Image>();
 
-			content.sprite = helpImage[currentIndex];
+			currentIndex = 0;
+			content.sprite = helpImage[currentIndex];			
 		}
 
 		public override void uiUpdate()
 		{
 			base.uiUpdate();
 
-			if (Input.anyKeyDown)
+			if (blockScrolling && !getUInputDown())
+				blockScrolling = false;
+
+			if (Input.GetAxisRaw("Horizontal") < 0f && !blockScrolling)
+				next();
+			else if (Input.GetAxisRaw("Horizontal") > 0f && !blockScrolling)
+				back();
+
+			if (Input.GetButtonDown("Cancel"))
 				ui.SetTrigger("Back");
 		}
 
 		public void back()
 		{
-			currentIndex = (int)Mathf.Repeat(currentIndex - 1, helpImage.Length);
-			content.sprite = helpImage[currentIndex];
+			currentIndex = (int)Mathf.Repeat(currentIndex + 1, helpImage.Length);
+			setPageIndex(currentIndex);		
 		}
 
 		public void next()
 		{
-			currentIndex = (int)Mathf.Repeat(currentIndex + 1, helpImage.Length);
-			content.sprite = helpImage[currentIndex];
+			currentIndex = (int)Mathf.Repeat(currentIndex - 1, helpImage.Length);
+			setPageIndex(currentIndex);
+		}
+
+		public void setPageIndex(int index)
+		{
+			content.sprite = helpImage[index];
+
+			foreach (Toggle indicator in this.transform.FindChild("Indicators").GetComponentsInChildren<Toggle>())
+			{
+				indicator.isOn = false;
+			}
+			this.transform.FindChild("Indicators").GetChild(index).GetComponent<Toggle>().isOn = true;
+
+			blockScrolling = true;
 		}
 	}
 }
