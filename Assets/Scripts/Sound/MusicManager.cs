2c4c37329e69110da0021f766e050b4107ae0d71
Blodjer <blodjer@live.de>
2015-02-01 17:22:01 +0100

    Clean up and fixes
    musicamanger clean up
    ui fixes

--- Assets/Scripts/Sound/MusicManager.cs
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -29,18 +29,20 @@ namespace PongJutsu
 		[SerializeField] private int pauseLayerElement = 0;
 		[SerializeField] private int peakLayerElement = 0;
 
-		[SerializeField] private Layer[] layers;
-
-		public static MusicManager current;
-
 		[System.Serializable]
 		public class Layer
 		{
 			public AudioSource source;
 			public AudioClip[] clips;
-			[HideInInspector] public int currentClipIndex = 0;
-			[HideInInspector] public int nextClipIndex = 0;
+			[HideInInspector]
+			public int currentClipIndex = 0;
+			[HideInInspector]
+			public int nextClipIndex = 0;
 		}
+
+		[SerializeField] private Layer[] layers;
+
+		public static MusicManager current;
 
 		void Awake()
 		{
@@ -55,22 +57,6 @@ namespace PongJutsu
 
 		void checkTrigger()
 		{
-			if (Input.GetKeyDown("f1"))
-			{
-				if (this.GetComponent<AudioSource>().mute)
-					this.GetComponent<AudioSource>().mute = false;
-				else
-					this.GetComponent<AudioSource>().mute = true;
-			}
-
-			if (Input.GetKeyDown("f2"))
-			{
-				if (AudioListener.pause)
-					AudioListener.pause = false;
-				else
-					AudioListener.pause = true;
-			}
-
 			if (Input.GetKeyDown("1"))
 				PlayPeakLayer();
 			else if (Input.GetKeyDown("2"))
@@ -97,39 +83,39 @@ namespace PongJutsu
 
 		public void PauseMusic()
 		{
-			foreach (AudioSource source in this.GetComponents<AudioSource>())
+			foreach (Layer layer in layers)
 			{
-				if (layers[pauseLayerElement].source != source)
-					StartCoroutine(IFadeout(source, 0.7f));
+				if (layers[pauseLayerElement].source != layer.source)
+					StartCoroutine(IFadeout(layer, 0.7f));
 			}
 		}
 
 		public void ResumeMusic()
 		{
-			foreach (AudioSource source in this.GetComponents<AudioSource>())
+			foreach (Layer layer in layers)
 			{
-				if (layers[pauseLayerElement].source != source)
-					StartCoroutine(IFadein(source, 1f));
+				if (layers[pauseLayerElement].source != layer.source)
+					StartCoroutine(IFadein(layer, 1f));
 			}
 		}
 
 		public void PlayPeakLayer()
 		{
-			StartCoroutine(IFadeout(layers[0].source, 0.5f));
-			StartCoroutine(IFadeout(layers[1].source, 0.5f));
-			Play(layers[peakLayerElement], layers[peakLayerElement].clips[0]);
+			StartCoroutine(IFadeout(layers[0], 0.5f));
+			StartCoroutine(IFadeout(layers[1], 0.5f));
+			Play(layers[peakLayerElement], 0);
 		}
 
 		public void StopPeakLayer()
 		{
-			StartCoroutine(IFadein(layers[0].source, 1f));
-			StartCoroutine(IFadein(layers[1].source, 1f));
-			StartCoroutine(IFadeout(layers[peakLayerElement].source, 1f));
+			StartCoroutine(IFadein(layers[0], 1f));
+			StartCoroutine(IFadein(layers[1], 1f));
+			StartCoroutine(IFadeout(layers[peakLayerElement], 1f));
 		}
 
-		private void Play(Layer layer, AudioClip clip)
+		private void Play(Layer layer, int clipIndex)
 		{
-			layer.source.clip = clip;
+			layer.source.clip = layer.clips[clipIndex];
 
 			layer.source.mute = false;
 			layer.source.volume = masterVolume;
@@ -140,7 +126,7 @@ namespace PongJutsu
 
 		private void StartLayer(Layer layer)
 		{
-			Play(layer, layer.clips[0]);
+			Play(layer, 0);
 		}
 
 		public void NextClipInLayer(Layer layer)
@@ -159,11 +145,13 @@ namespace PongJutsu
 		IEnumerator WaitForNextClip(Layer layer)
 		{
 			yield return new WaitForSeconds(layer.source.clip.length - layer.source.time);
-			Play(layer, layer.clips[layer.nextClipIndex]);
+			Play(layer, layer.nextClipIndex);
 		}
 
-		IEnumerator IFadein(AudioSource source, float duration)
+		IEnumerator IFadein(Layer layer, float duration)
 		{
+			AudioSource source = layer.source;
+
 			source.Play();
 			while (source.volume < masterVolume)
 			{
@@ -173,8 +161,10 @@ namespace PongJutsu
 			}
 		}
 
-		IEnumerator IFadeout(AudioSource source, float duration)
+		IEnumerator IFadeout(Layer layer, float duration)
 		{
+			AudioSource source = layer.source;
+
 			while (source.volume > 0f)
 			{
 				float volume = source.volume - (masterVolume / duration) * Time.unscaledDeltaTime;
