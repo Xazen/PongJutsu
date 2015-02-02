943939ebc49f230068a0ac5a9efa35e7ea74e656
Blodjer <blodjer@live.de>
2015-02-03 00:11:43 +0100

    Add placeholder scores

--- Assets/Scripts/UI/MatchResultScreen.cs
+++ Assets/Scripts/UI/MatchResultScreen.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 namespace PongJutsu
 {
 	public class MatchResultScreen : UIScript
 	{
+		[SerializeField]
+		private ScoreOutput scoreOutputLeft;
+		[SerializeField]
+		private ScoreOutput scoreOutputRight;
+
 		public void click_Exit()
 		{
 			GameManager.ExitGame();
@@ -14,5 +20,22 @@ namespace PongJutsu
 		{
 			GameManager.RestartGame();
 		}
+	}
+
+	[System.Serializable]
+	class ScoreOutput
+	{
+		[SerializeField]
+		Text wins;
+		[SerializeField]
+		Text reflected;
+		[SerializeField]
+		Text catched;
+		[SerializeField]
+		Text itemhit;
+		[SerializeField]
+		Text forthit;
+		[SerializeField]
+		Text dealtdamage;
 	}
 }
