e79b0d1a9aac07dca56998d4f24f6bb5a293cd6a
Blodjer <blodjer@live.de>
2015-02-03 11:52:31 +0100

    Split GameManager into GameMatch class

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,7 +1,5 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
 using System.Collections;
-using System.Collections.Generic;
 
 namespace PongJutsu
 {
@@ -236,43 +234,6 @@ namespace PongJutsu
 		{
 			GameObject.Find("Arena").GetComponent<AudioSource>().Stop();
 			ui.SetTrigger("ExitGame");
-		}
-	}
-
-	public class GameMatch : MonoBehaviour
-	{
-		private static List<string> results = new List<string>();
-		private static int minRoundsToWin = 2;
-
-		public static void newMatch()
-		{
-			results.Clear();
-			GameScore.Clear();
-		}
-
-		public static void addWinner(string winner)
-		{
-			results.Add(winner);
-		}
-
-		public static List<string> getWinnerList()
-		{
-			return results;
-		}
-
-		public static string getLastWinner()
-		{
-			return results[results.Count - 1];
-		}
-
-		public static string getMatchWinner()
-		{
-			if (results.FindAll(x => x == "left").Count >= minRoundsToWin)
-				return "left";
-			else if (results.FindAll(x => x == "right").Count >= minRoundsToWin)
-				return "right";
-			else
-				return null;
 		}
 	}
 }
