4cb4b2930e45736e10f2e2d3e5fd229dd774f924
Blodjer <blodjer@live.de>
2015-02-09 18:36:34 +0100

    Add music volume control

---
+++ Assets/Scripts/Sound/AudioVolumeControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PongJutsu
+{
+	public class AudioVolumeControl : MonoBehaviour
+	{
+		private float sourceVolume;
+
+		void Start()
+		{
+			sourceVolume = this.GetComponent<AudioSource>().volume;
+		}
+
+		void Update() 
+		{
+			this.GetComponent<AudioSource>().volume = sourceVolume * MusicManager.current.masterVolume;
+		}
+	}
+}
