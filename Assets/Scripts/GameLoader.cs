6e2c86248ca9f70b49ba6e93b8b117b2621fac7a
Blodjer <blodjer@live.de>
2015-02-11 17:43:03 +0100

    Add loading screen

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLoader : MonoBehaviour 
+{
+	[SerializeField] private string levelname = "PongJutsu";
+
+	[SerializeField] private Text loadingtext;
+
+	private AsyncOperation async = null;
+
+	void Awake()
+	{
+		loadingtext.text = "";
+
+		if (levelname != "")
+			StartCoroutine(LoadLevel(levelname));
+	}
+
+	void Update()
+	{
+		if (async != null)
+			loadingtext.text = "Load " + levelname + "... " + Mathf.RoundToInt(async.progress * 100) + "%";
+	}
+
+	private IEnumerator LoadLevel(string level)
+	{
+		yield return new WaitForSeconds(1f);
+		async = Application.LoadLevelAsync(level);
+		yield return async;
+	}
+}
