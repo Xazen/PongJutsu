cad6f13c0a6747433e6f7c5462134abe4774c1a6
Blodjer <blodjer@live.de>
2015-02-17 13:15:55 +0100

    Rescript Player class

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,43 +3,55 @@ using System.Collections;
 
 namespace PongJutsu
 {
+	public enum PlayerSide
+	{
+		Left,
+		Right
+	}
+
 	public class Player : PlayerBase
 	{
+		PlayerSide _playerSide;
+		public PlayerSide playerSide
+		{
+			get
+			{
+				return _playerSide;
+			}
+			set
+			{
+				_playerSide = value;
+
+				if (_playerSide == PlayerSide.Left)
+				{
+					direction = 1;
+					this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+					this.GetComponent<Animator>().runtimeAnimatorController = ninjaLeftController;
+					forts = GameObject.FindGameObjectsWithTag("FortLeft");
+				}
+				else if (_playerSide == PlayerSide.Right)
+				{
+					direction = -1;
+					this.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+
+					this.GetComponent<Animator>().runtimeAnimatorController = ninjaRightController;
+					forts = GameObject.FindGameObjectsWithTag("FortRight");
+				}
+			}
+		}
+
 		public AnimatorOverrideController ninjaLeftController;
 		public AnimatorOverrideController ninjaRightController;
 
 		[HideInInspector] public GameObject[] forts;
 
-		public bool mirror = false;
 		[HideInInspector] public int direction = 1;
 
 		[HideInInspector] public int comboCount = 0;
 
-		[SerializeField]
-		private float resetComboTime = 4.0f;
+		[SerializeField] private float resetComboTime = 4.0f;
 		private float passedTimeSinceCombo = 0.0f;
-
-		public void Setup()
-		{
-			if (mirror)
-			{
-				// Mirror the Player
-				direction = -1;
-				this.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-			}
-
-			// Setup different players
-			if (this.tag == "PlayerLeft")
-			{
-				this.GetComponent<Animator>().runtimeAnimatorController = ninjaLeftController;
-				forts = GameObject.FindGameObjectsWithTag("FortLeft");
-			}
-			else if (this.tag == "PlayerRight")
-			{
-				this.GetComponent<Animator>().runtimeAnimatorController = ninjaRightController;
-				forts = GameObject.FindGameObjectsWithTag("FortRight");
-			}
-		}
 
 		public void Update()
 		{
